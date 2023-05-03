7d073deb78dd54220629b48e92ae130f6d0c60ac
Mike Heavers <mheavers@gmail.com>
2023-05-03 13:29:28 -0700

    add client id, change callback

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,49 +1,53 @@
-gapi.analytics.ready(function () {
-  // Step 3: Authorize the user.
+function handleCredentialResponse(e) {
+  console.log("auth", e);
+}
 
-  // var CLIENT_ID = "108901188668516974494";
-  const CLIENT_ID =
-    "123270247522-vkcb0qsa4dcoa9tdjd1kbolb00q71fim.apps.googleusercontent.com";
+// gapi.analytics.ready(function () {
+//   // Step 3: Authorize the user.
 
-  gapi.analytics.auth.authorize({
-    container: "auth-button",
-    clientid: CLIENT_ID,
-  });
+//   // var CLIENT_ID = "108901188668516974494";
+//   const CLIENT_ID =
+//     "123270247522-vkcb0qsa4dcoa9tdjd1kbolb00q71fim.apps.googleusercontent.com";
 
-  // Step 4: Create the view selector.
+//   gapi.analytics.auth.authorize({
+//     container: "auth-button",
+//     clientid: CLIENT_ID,
+//   });
 
-  var viewSelector = new gapi.analytics.ViewSelector({
-    container: "view-selector",
-  });
+//   // Step 4: Create the view selector.
 
-  // Step 5: Create the timeline chart.
+//   var viewSelector = new gapi.analytics.ViewSelector({
+//     container: "view-selector",
+//   });
 
-  var timeline = new gapi.analytics.googleCharts.DataChart({
-    reportType: "ga",
-    query: {
-      dimensions: "ga:date",
-      metrics: "ga:sessions",
-      "start-date": "30daysAgo",
-      "end-date": "yesterday",
-    },
-    chart: {
-      type: "LINE",
-      container: "timeline",
-    },
-  });
+//   // Step 5: Create the timeline chart.
 
-  // Step 6: Hook up the components to work together.
+//   var timeline = new gapi.analytics.googleCharts.DataChart({
+//     reportType: "ga",
+//     query: {
+//       dimensions: "ga:date",
+//       metrics: "ga:sessions",
+//       "start-date": "30daysAgo",
+//       "end-date": "yesterday",
+//     },
+//     chart: {
+//       type: "LINE",
+//       container: "timeline",
+//     },
+//   });
 
-  gapi.analytics.auth.on("success", function (response) {
-    viewSelector.execute();
-  });
+//   // Step 6: Hook up the components to work together.
 
-  viewSelector.on("change", function (ids) {
-    var newIds = {
-      query: {
-        ids: ids,
-      },
-    };
-    timeline.set(newIds).execute();
-  });
-});
+//   gapi.analytics.auth.on("success", function (response) {
+//     viewSelector.execute();
+//   });
+
+//   viewSelector.on("change", function (ids) {
+//     var newIds = {
+//       query: {
+//         ids: ids,
+//       },
+//     };
+//     timeline.set(newIds).execute();
+//   });
+// });
