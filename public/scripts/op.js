512a3854cc8d9cd75cc68ec93c821f6ea2ad0119
Mike Heavers <mheavers@gmail.com>
2023-09-04 09:12:44 -0700

    modal trigger

--- public/scripts/op.js
+++ public/scripts/op.js
@@ -1,13 +1,25 @@
 console.log("op.js loaded");
 
+const toggleRequestModal = function (isActive) {
+  document.body.classList.toggle("js__request-active", isActive);
+};
+
 const onRequestButtonClick = function () {
-  document.body.classList.toggle("js__request-active");
+  toggleRequestModal(true);
+};
+
+const onRequestCloseClick = function () {
+  toggleRequestModal(false);
 };
 
 const addEventListeners = function () {
   document
     .getElementById("request__button")
     .addEventListener("click", onRequestButtonClick);
+
+  document
+    .getElementById("request__button-close")
+    .addEventListener("click", onRequestCloseClick);
 };
 
 const init = function () {
